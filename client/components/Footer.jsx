09a05e511b8f2bf6b6a26cd76a512b1a23393d08
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-10-12 16:25:16 +1300

    home page is ready to go

---
+++ client/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Footer = () => {
+    return (
+      <div id='main footer' className='title'style={{textAlign:'center', marginTop:'5vw'}}>
+        <h4 id='footer-title' style={{backgroundColor: 'pink', color:'white'}}>This app is brougth to you by <a href='https://github.com/marie-phu-qui'>Marie Nguyen Phu Qui</a>. For more information about the game developpement perspectives of this app, please consult the <a href='https://github.com/marie-phu-qui/boop-the-sheep/blob/master/README.md'>ReadMe</a></h4>
+      </div>
+    )
+  }
+
+export default Footer
---
+++ client/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Header = () => {
+    return (
+      <div id='main header' className='title'style={{textAlign:'center', marginBottom:'5vw'}}>
+        <h1 id='header-title' style={{color: 'pink'}}>BOOP THE SHEEP</h1>
+      </div>
+    )
+  }
+
+export default Header
