42d43c184b73b7de108c7bc0b0ccf2748cff177e
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-10-25 10:49:11 +1300

    sticky with bootstrap but not responsive enougth

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,12 +6,12 @@ import Footer from "./Footer";
 const App = () => {
     return (
       <React.Fragment>
-        <div className='app-content' style={{flex: '1 0 auto',minHeight: '100%'}}>
+        <main role="main" className="container">
             <Header />
             <div style={{alignItems: 'center'}}>
                 <img src='https://ubisafe.org/images/sheep-vector-1.png' style={{display:'block', margin: 'auto'}}/>
             </div>
-        </div>
+        </main>
         <Footer />
       </React.Fragment>
     )
